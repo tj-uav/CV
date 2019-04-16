592eb1c87b589ac94c4efc828916f588c4f61679
Srikar Gouru <srikarg89@gmail.com>
2019-04-15 23:03:39 -0400

    To run on school computer

--- CNN/alphanumeric/train_CNN_EMNIST.py
+++ CNN/alphanumeric/train_CNN_EMNIST.py
@@ -8,19 +8,8 @@ import numpy as np
 from random import shuffle
 import cv2
 
-#Dicts for classes
-classes_dict = {}
-classes_dict["digits"] = 10
-classes_dict["letters"] = 26
-digits_map = [i for i in range(10)]
-letters_map = [chr(i+65) for i in range(26)]
-outputs_dict = {}
-outputs_dict["digits"] = digits_map
-outputs_dict["letters"] = letters_map
-
 matfiles = []
-matfiles.append((loadmat("C:/Users/Srikar/Documents/NN_Databases/EMNIST/matlab/emnist-digits.mat"),"digits"))
-matfiles.append((loadmat("C:/Users/Srikar/Documents/NN_Databases/EMNIST/matlab/emnist-letters.mat"),"letters"))
+matfiles.append(loadmat("C:/Users/Srikar/Documents/NN_Databases/EMNIST/matlab/emnist-byclass.mat"))
 
 output_map = []
 
@@ -30,21 +19,18 @@ def rotate(img):
     flipped = np.fliplr(img)
     return np.rot90(flipped)
 
-
 height = 28
 width = 28
 
-def get_data(matfiles):
-    num_classes = 0
-    thresh = 120
+def get_data(matfile):
+    num_classes = 36
+    thresh = 80
     total_x_train = np.array([])
     total_y_train = np.array([])
     total_x_test = np.array([])
     total_y_test = np.array([])
-    for count,(matfile,name) in enumerate(matfiles):
-        print(name)
-        nbclasses = classes_dict[name]
-        output_map.extend(outputs_dict[name])
+    for count,matfile, in enumerate(matfiles):
+#        print(name)
         data = matfile['dataset']
 
         x_train = data['train'][0,0]['images'][0,0]
@@ -52,31 +38,15 @@ def get_data(matfiles):
         x_test = data['test'][0,0]['images'][0,0]
         y_test = data['test'][0,0]['labels'][0,0]
 
+        y_train = y_train - 1
+        y_test = y_test - 1
+
         print("Removing validation set")
-
-
         val_start = x_train.shape[0] - x_test.shape[0]
         x_val = x_train[val_start:x_train.shape[0],:]
         y_val = y_train[val_start:x_train.shape[0]]
         x_train = x_train[0:val_start,:]
         y_train = y_train[0:val_start]
-
-        diff = num_classes
-        if name == "letters":
-            diff -= 1
-
-        if diff != 0:
-            for i in range(len(y_train)):
-                j = y_train[i][0]
-                j += diff
-                y_train[i] = np.array([j],dtype='uint8')
-
-            for i in range(len(y_test)):
-                j = y_test[i][0]
-                j += diff
-                y_test[i] = np.array([j],dtype='uint8')
-
-        num_classes += nbclasses
 
         if count == 0:
             total_x_train = x_train
@@ -100,25 +70,50 @@ def get_data(matfiles):
     total_x_train = total_x_train.reshape(len(total_x_train), height, width, 1)
     total_x_test = total_x_test.reshape(len(total_x_test), height, width, 1)
 
+    print(x_train.shape)
+    print(y_train.shape)
+    print(x_test.shape)
+    print(y_test.shape)
+
     print("Rotating images")
 
+    toRemove = []
     for i in range(len(total_x_train)):
+        if total_y_train[i][0] >= 36:
+            toRemove.append(i)
         total_x_train[i] = rotate(total_x_train[i])
 
+    print(len(toRemove))
+
+    total_x_train = np.delete(total_x_train,toRemove,0)
+    total_y_train = np.delete(total_y_train,toRemove,0)
+
+    toRemove = []
     for i in range(len(total_x_test)):
+        if total_y_test[i][0] >= 36:
+            toRemove.append(i)
         total_x_test[i] = rotate(total_x_test[i])
 
-    print("Shuffling data sets")
 
-    indices_train = np.arange(total_x_train.shape[0])
-    np.random.shuffle(indices_train)
-    total_x_train = total_x_train[indices_train]
-    total_y_train = total_y_train[indices_train]
+    total_x_test = np.delete(total_x_test,toRemove,0)
+    total_y_test = np.delete(total_y_test,toRemove,0)
 
-    indices_test = np.arange(total_x_test.shape[0])
-    np.random.shuffle(indices_test)
-    total_x_test = total_x_test[indices_test]
-    total_y_test = total_y_test[indices_test]
+    print(total_x_train.shape)
+    print(total_y_train.shape)
+    print(total_x_test.shape)
+    print(total_x_test.shape)
+
+#    print("Shuffling data sets")
+
+#    indices_train = np.arange(total_x_train.shape[0])
+#    np.random.shuffle(indices_train)
+#    total_x_train = total_x_train[indices_train]
+#    total_y_train = total_y_train[indices_train]
+
+#    indices_test = np.arange(total_x_test.shape[0])
+#    np.random.shuffle(indices_test)
+#    total_x_test = total_x_test[indices_test]
+#    total_y_test = total_y_test[indices_test]
 
     total_y_train = np_utils.to_categorical(total_y_train, num_classes)
     total_y_test = np_utils.to_categorical(total_y_test, num_classes)
@@ -133,12 +128,11 @@ def make_model(num_classes):
     kernel_size = (3, 3) # convolution kernel size
     input_shape = (height,width,1)
     model = Sequential()
-    model.add(Convolution2D(nb_filters,
+    model.add(Convolution2D(32,
                             kernel_size,
-                            padding='valid',
                             input_shape=input_shape,
                             activation='relu'))
-    model.add(Convolution2D(nb_filters,
+    model.add(Convolution2D(64,
                             kernel_size,
                             activation='relu'))
 
@@ -162,9 +156,8 @@ x_train, y_train, x_test, y_test, num_classes = get_data(matfiles)
 print("Generating model")
 print("Classes: " + str(num_classes))
 model = make_model(num_classes)
-epochs = 2
+epochs = 10
 batch_size = 256
-
 model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
@@ -174,8 +167,8 @@ model.fit(x_train, y_train,
 model.summary()
 # serialize model to JSON
 model_json = model.to_json()
-with open("alphanumeric_model.json", "w") as json_file:
+with open("alphanumeric_model2.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model.save_weights("alphanumeric_model.h5")
+model.save_weights("alphanumeric_model2.h5")
 print("Saved model to disk")
