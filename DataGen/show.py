708fc5f6c7307d59ea9953c5bfd5d6e21d2f8ab6
Black <1473127@fcps.edu>
2020-01-18 17:26:51 -0500

    Removes all files from master to allow reintroduction of files. All the files deleted here still exist in the reorganization branch.

--- DataGen/show.py
+++
@@ -1,9 +0,0 @@
-cursor = [0,0]
-for i in range(250,10):
-    img = cv2.imread("Hue " + str(i) + ".png")
-    cv2.circle(img, cursor, , (0,0,0), 3)
-    cv2.imshow()
-    cv2.waitKey(0)
-
-cv2.destroyAllWindows()
-
--- OpenCVscripts/getRGBColors.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-request = requests.get('http://chir.ag/projects/name-that-color/#3C7890')
-text = request.text
-print(text)
