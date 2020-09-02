0017976b93d9490186c03464f513261d42fdc41d
unknown <dstefanescu1609@gmail.com>
2020-09-02 16:03:59 -0700

    Now stitching files in the images folder

--- Processing/Image Stitching/image_stitching_opencv.py
+++ Processing/Image Stitching/image_stitching_opencv.py
@@ -1,23 +1,28 @@
+#To run code you must have a directory at Processing/Image Stitching/Images/ containing images
+
+#Imports
 import cv2
 import numpy as np
+import os
 
+#Reading image files
+images = []
+imagesDirectory = 'Processing/Image Stitching/Images/'
 dim = (1024,768)
-left=cv2.imread('Processing/Image Stitching/Left.jpg',cv2.IMREAD_COLOR)
-left = cv2.resize(left,dim,interpolation=cv2.INTER_AREA)
-right=cv2.imread('Processing/Image Stitching/Right.jpg',cv2.IMREAD_COLOR)
-right=cv2.resize(right,dim,interpolation=cv2.INTER_AREA)
+for fileName in os.listdir(imagesDirectory):
+    fileDir = os.path.join(imagesDirectory, fileName)
+    image=cv2.imread(fileDir,cv2.IMREAD_COLOR)
+    image=cv2.resize(image,dim,interpolation=cv2.INTER_AREA)
+    images.append(image)
 
-images =[]
-images.append(left)
-images.append(right)
-
+#Settings up image stiticer
 stitcher = cv2.Stitcher.create()
 ret, pano = stitcher.stitch(images)
 
+#Display stitched image
 if ret ==cv2.STITCHER_OK:
     cv2.imshow('Panorama', pano)
-    cv2.waitKey()
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
     print("Error during stitching")
-
