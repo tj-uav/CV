fd1852d6f518c3f8460d12bff2ecfaa8f20a1949
Srikar Gouru <srikarg89@gmail.com>
2020-11-14 11:46:45 -0500

    Remove resize method

--- Processing/Image Stitching/image_stitching_opencv.py
+++ Processing/Image Stitching/image_stitching_opencv.py
@@ -7,24 +7,32 @@ import os, signal
 
 #Reading image files
 images = []
-imagesDirectory = os.getcwd() + '/Processing/Image Stitching/Images/'
-dim = (1024,768)
+#imagesDirectory = os.getcwd() + '/Processing/Image Stitching/Images/'
+imagesDirectory = os.getcwd() + '/Images/'
+# dim = (1024, 768)
 for fileName in os.listdir(imagesDirectory):
     fileDir = os.path.join(imagesDirectory, fileName)
     image=cv2.imread(fileDir,cv2.IMREAD_COLOR)
-    image=cv2.resize(image,dim,interpolation=cv2.INTER_AREA)
+    # image=cv2.resize(image,dim,interpolation=cv2.INTER_AREA)
     images.append(image)
 
+print("Image sizes:", images[0].shape, images[1].shape)
 #Settings up image stiticer
+print("Running image stitcher")
 stitcher = cv2.Stitcher.create()
 ret, pano = stitcher.stitch(images)
 
+print("Done stitching, saving to file")
 #Display stitched image
-if ret ==cv2.STITCHER_OK:
-    cv2.imshow('Panorama', pano)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+if ret == cv2.STITCHER_OK:
+    cv2.imwrite("panorama.png", pano)
 else:
-    print("Error during stitching")
+    print("Error during stitching:", ret)
+    if ret == cv2.STITCHER_ERR_NEED_MORE_IMGS:
+        print("Need more images")
+    if ret == cv2.STITCHER_ERR_HOMOGRAPHY_EST_FAIL:
+        print("Homography est failed")
+    if ret == cv2.STITCHER_ERR_CAMERA_PARAMS_ADJUST_FAIL:
+        print("Camera params adjust failed")
 
 os.kill (os.getpid(), signal.SIGTERM)
