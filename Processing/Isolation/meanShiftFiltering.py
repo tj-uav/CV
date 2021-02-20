6ab5aacc46fe562896cc58a4f844cdd76789a8ea
unknown <dstefanescu1609@gmail.com>
2021-02-20 11:40:01 -0500

    Added blob detection

--- Processing/Isolation/meanShiftFiltering.py
+++ Processing/Isolation/meanShiftFiltering.py
@@ -6,14 +6,31 @@ import time
 
 
 startTime = time.time()
-imageDir = "pic588.jpg"
+imageDir = "1.jpg"
 image = cv2.imread(imageDir)
-meansImage = cv2.pyrMeanShiftFiltering(image, 20, 70)
+image = cv2.resize(image, (800, 600))
+meansImage = cv2.pyrMeanShiftFiltering(image, 30, 70)
 edgeImage = cv2.Canny(meansImage,100,200)
-edgeImage = cv2.resize(edgeImage, (500, 1000))
-meansImage = cv2.resize(meansImage, (500, 1000))
+
+kernel = np.ones((5,5),np.uint8)
+closing = cv2.morphologyEx(edgeImage, cv2.MORPH_CLOSE, kernel)
+
+params = cv2.SimpleBlobDetector_Params()
+params.minThreshold = 10
+params.maxThreshold = 200
+params.filterByColor = True
+params.blobColor = 255
+params.filterByArea = False
+
+detector = cv2.SimpleBlobDetector_create(params)
+keypoints = detector.detect(closing)
+im_with_keypoints = cv2.drawKeypoints(edgeImage, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow("lol", im_with_keypoints)
+
+cv2.imshow("Closing", closing)
 print (time.time() - startTime)
 cv2.imshow("yuh", edgeImage)
 cv2.imshow("yuhyuh", meansImage)
+cv2.imshow("image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
