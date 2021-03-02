409b9817eff54541c044a08f5d722f8f3744e208
unknown <dstefanescu1609@gmail.com>
2021-03-02 00:43:55 -0500

    No false negatives, implemented new connected component filtering algorithm

--- Processing/Isolation/meanShiftFiltering.py
+++ Processing/Isolation/meanShiftFiltering.py
@@ -19,10 +19,14 @@ def prepareEdgeImage(edgeImage):
     """
     Applies morphological transformations to binary (edge) image.
     """
-    kernel = np.ones((3,3),np.uint8)
+    kernel = np.ones((2,2),np.uint8)
     dilated_img = cv2.dilate(edgeImage, kernel, iterations = 2)
-    kernel = np.ones((5,5),np.uint8)
-    closing = cv2.morphologyEx(dilated_img, cv2.MORPH_CLOSE, kernel)
+
+    smallComponentsImage = removeLargeConnectedComponents(dilated_img, 300)
+    cv2.imshow("Small Connected Component Image", smallComponentsImage)
+
+    kernel = np.ones((10,10),np.uint8)
+    closing = cv2.morphologyEx(smallComponentsImage, cv2.MORPH_CLOSE, kernel)
     return closing
 
 def blobDetection(image):
@@ -50,38 +54,34 @@ def blobDetection(image):
 def getTargetImages(targetCoordinates, closingImage):
     return
 
-def removeLargeConnectedComponents(image):
+def removeLargeConnectedComponents(image, max_size):
     #find all your connected components (white blobs in your image)
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
     #connectedComponentswithStats yields every seperated component with information on each of them, such as size
     #the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
-    # minimum size of particles we want to keep (number of pixels)
-    #here, it's a fixed value, but you can set it as you want, eg the mean of the sizes or whatever
-    min_size = 150
-
     #your answer image
     img2 = np.zeros((output.shape))
     #for every component in the image, you keep it only if it's above min_size
     for i in range(0, nb_components):
-        if sizes[i] <= min_size:
+        if sizes[i] <= max_size:
             img2[output == i + 1] = 255
     return img2.astype(np.uint8)
 
 def findTargets(image):
     startTime = time.time()
-    image = cv2.resize(image, (800, 600))
+    image = cv2.resize(image, (1200, 800))
+    cv2.imshow("Origional", image)
     meansImage = cv2.pyrMeanShiftFiltering(image, 30, 70)
     edgeImage = cv2.Canny(meansImage,100,200)
-    smallComponentImage = removeLargeConnectedComponents(edgeImage)
-    cv2.imshow("Test", smallComponentImage)
-    closingImage = prepareEdgeImage(smallComponentImage)
+    closingImage = prepareEdgeImage(edgeImage)
     targetCoordinates, blobImage = blobDetection(closingImage)
+    im_with_keypoints = cv2.drawKeypoints(image, targetCoordinates, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     print ("Detection Procesing Time:", str(time.time() - startTime))
-    return (blobImage)
+    return (im_with_keypoints)
 
-imageDir = "sampleTest.jpg"
+imageDir = "8.jpg"
 image = cv2.imread(imageDir)
 finalImage = findTargets(image)
 cv2.imshow("Final", finalImage)
