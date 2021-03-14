15aa076450c7b90bc80afbed96de6fdbfb763f5b
unknown <dstefanescu1609@gmail.com>
2021-03-14 12:00:37 -0400

    findTargets now returns cropped possible target locations

--- Processing/Isolation/meanShiftFiltering.py
+++ Processing/Isolation/meanShiftFiltering.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 import os
 import time
+from matplotlib import pyplot as plt
 
 # des = cv2.bitwise_not(meansImage)
 # contour,hier = cv2.findContours(des,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
@@ -23,7 +24,7 @@ def prepareEdgeImage(edgeImage):
     dilated_img = cv2.dilate(edgeImage, kernel, iterations = 2)
 
     smallComponentsImage = removeLargeConnectedComponents(dilated_img, 300)
-    cv2.imshow("Small Connected Component Image", smallComponentsImage)
+    #cv2.imshow("Small Connected Component Image", smallComponentsImage)
 
     kernel = np.ones((10,10),np.uint8)
     closing = cv2.morphologyEx(smallComponentsImage, cv2.MORPH_CLOSE, kernel)
@@ -51,39 +52,46 @@ def blobDetection(image):
     im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     return (keypoints, im_with_keypoints)
 
-def getTargetImages(targetCoordinates, closingImage):
-    return
+def getTargetImages(targetCoordinates, image, bigImage):
+    croppedImages = []
+    padding = 5
+    for coord in targetCoordinates:
+        x,y= coord.pt
+        radius = (coord.size//2) + padding
+        maxX = int((x + radius)/image.shape[1] * bigImage.shape[1])
+        minX = int((x - radius)/image.shape[1] * bigImage.shape[1])
+        maxY = int((y + radius)/image.shape[0] * bigImage.shape[0])
+        minY = int((y - radius)/image.shape[0] * bigImage.shape[0])
+        crop = bigImage[minY:maxY, minX:maxX]
+        croppedImages.append(crop)
+    return croppedImages
 
 def removeLargeConnectedComponents(image, max_size):
-    #find all your connected components (white blobs in your image)
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
-    #connectedComponentswithStats yields every seperated component with information on each of them, such as size
-    #the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
-    #your answer image
     img2 = np.zeros((output.shape))
-    #for every component in the image, you keep it only if it's above min_size
     for i in range(0, nb_components):
         if sizes[i] <= max_size:
             img2[output == i + 1] = 255
     return img2.astype(np.uint8)
 
-def findTargets(image):
+def findTargets(origionalImage):
     startTime = time.time()
-    image = cv2.resize(image, (800, 600))
-    cv2.imshow("Origional", image)
+    image = cv2.resize(origionalImage, (800, 600))
+    #cv2.imshow("Origional", image)
     meansImage = cv2.pyrMeanShiftFiltering(image, 30, 70)
     edgeImage = cv2.Canny(meansImage,100,200)
     closingImage = prepareEdgeImage(edgeImage)
     targetCoordinates, blobImage = blobDetection(closingImage)
     im_with_keypoints = cv2.drawKeypoints(image, targetCoordinates, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     print ("Detection Procesing Time:", str(time.time() - startTime))
-    return (im_with_keypoints)
+    targets = getTargetImages(targetCoordinates, image, origionalImage)
+    return (im_with_keypoints, targets)
 
-imageDir = "yuh.jpg"
+imageDir = "2.jpg"
 image = cv2.imread(imageDir)
-finalImage = findTargets(image)
+finalImage, targets = findTargets(image)
 cv2.imshow("Final", finalImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
